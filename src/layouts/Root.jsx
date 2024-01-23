315d4fa8c6a246c7ba70ce9b3cf31afb93fa37d9
Md Noble <mdnoble.wd@gmail.com>
2024-01-23 23:11:56 +0600

    created basic folder structure and basic routing

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <section>
+            <Outlet></Outlet>
+        </section>
+    );
+};
+
+export default Root;
