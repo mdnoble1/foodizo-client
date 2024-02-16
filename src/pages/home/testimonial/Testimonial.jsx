4272fe61a4a6a26c5a9b77e9155123c5ccefbd79
Md Noble <mdnoble.wd@gmail.com>
2024-02-16 20:41:36 +0600

    loaded review data and added swiper js to display review

--- src/pages/home/testimonial/Testimonial.jsx
+++ src/pages/home/testimonial/Testimonial.jsx
@@ -1,12 +1,53 @@
 import SectionTitle from "../../../components/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 const Testimonial = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("review.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
   return (
     <section>
       <SectionTitle
         title={"Inspiring Experiences"}
         subtitle={"Memories-Made-Flavorful"}
       ></SectionTitle>
+
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"3"}
+        coverflowEffect={{
+          rotate: 30,
+          stretch: 50,
+          depth: 200,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper w-11/12 mx-auto my-10 lg:my-16"
+      >
+        {reviews?.map((review) => (
+          <SwiperSlide key={review.id} className="">
+            <div>
+              <img className="w-full mx-auto" src={review.image} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
