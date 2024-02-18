02f6aaeca5ddaba5bc64ab8b13fb9cdf09df21c3
Md Noble <mdnoble.wd@gmail.com>
2024-02-18 21:02:00 +0600

    updated review card style

--- src/pages/home/testimonial/Testimonial.jsx
+++ src/pages/home/testimonial/Testimonial.jsx
@@ -36,9 +36,9 @@ const Testimonial = () => {
           disableOnInteraction: true,
         }}
         coverflowEffect={{
-          rotate: 20,
+          rotate: 10,
           stretch: 50,
-          depth: 100,
+          depth: 50,
           modifier: 1,
           slideShadows: false,
         }}
@@ -50,16 +50,23 @@ const Testimonial = () => {
         }}
         pagination={true}
         modules={[EffectCoverflow, Pagination, Autoplay]}
-        className="mySwiper w-11/12 md:w-3/5 lg:w-10/12 mx-auto my-4 lg:my-8"
+        className="mySwiper w-10/12 md:w-3/5 lg:w-10/12 mx-auto my-4 lg:my-8"
       >
         {reviews?.map((review) => (
-          <SwiperSlide key={review.id} className="py-12 px-8 md:px-2 lg:py-12 lg:px-16">
-            <div className="md:w-10/12 lg:w-3/4 mx-auto bg-gray-200 px-4 py-10 rounded-xl drop-shadow-xl">
+          <SwiperSlide
+            key={review.id}
+            className="py-12 px-8 md:px-2 lg:py-12 lg:px-16"
+          >
+            <div className="md:w-10/12 lg:w-3/4 mx-auto bg-[#fffbef] px-4 pt-10 rounded-xl drop-shadow-xl border border-white hover:border-1 hover:border-[#ff5900] duration-500 ease-in-out">
               <div className="w-fit bg-[#ff5900] p-1 rounded-full mx-auto">
                 <img className="w-32 mx-auto rounded-full" src={review.image} />
               </div>
-              <h2 className="font-semibold lg:text-lg text-black uppercase text-center my-4">{review.name}</h2>
-              <p className="text-sm lg:text-base text-gray-500 text-center h-18 md:h-24 mb-8">{review.review}</p>
+              <h2 className="font-semibold lg:text-lg text-black uppercase text-center my-4">
+                {review.name}
+              </h2>
+              <p className="text-sm lg:text-base text-gray-500 text-center h-28 md:h-36 mb-8">
+                {review.review}
+              </p>
             </div>
           </SwiperSlide>
         ))}
