d2e1490b5047a810950837d76adb9d5fcfb9065b
Md Noble <mdnoble.wd@gmail.com>
2024-02-19 23:51:30 +0600

    sliced populer menu data to show only 6 items

--- src/pages/home/popularMenu/PopularMenu.jsx
+++ src/pages/home/popularMenu/PopularMenu.jsx
@@ -1,21 +1,28 @@
 import SectionTitle from "../../../components/SectionTitle";
 import MenuItemCard from "../../../components/MenuItemCard";
 import useMenu from "../../../hooks/useMenu";
+import { Link } from "react-router-dom";
+import MenuButton from "../../../components/MenuButton";
 
 const PopularMenu = () => {
   const [menu] = useMenu();
   const popularItems = menu.filter((item) => item.category == "popular");
 
   return (
-    <section>
+    <section className="my-10 lg:my-16">
       <SectionTitle
         title={"Popular Palates"}
         subtitle={"Savor-Every-Bite"}
       ></SectionTitle>
-      <div className="w-11/12 mx-auto my-10 lg:my-16 grid md:grid-cols-2">
-        {popularItems?.map((item) => (
+      <div className="w-11/12 mx-auto mt-10 md:mt-16 mb-4 md:mb-6 grid md:grid-cols-2">
+        {popularItems?.slice(0, 6).map((item) => (
           <MenuItemCard key={item.id} item={item}></MenuItemCard>
         ))}
+      </div>
+      <div className="text-center">
+        <Link to="/shop/offered">
+          <MenuButton buttonTitle={"Show All"}></MenuButton>
+        </Link>
       </div>
     </section>
   );
--- src/pages/menu/TodayOffer/TodayOffer.jsx
+++ src/pages/menu/TodayOffer/TodayOffer.jsx
@@ -5,18 +5,18 @@ import MenuButton from "../../../components/MenuButton";
 
 const TodayOffer = ({ items }) => {
   return (
-    <section className="lg:mt-32 mb-20">
+    <section className="my-10 lg:my-16">
       <SectionTitle
         title={"Special Offerings"}
         subtitle={"Daily-Dish-Delicacies"}
       ></SectionTitle>
-      <div className="w-11/12 mx-auto my-10 lg:my-16 grid md:grid-cols-2">
-        {items?.map((item) => (
+      <div className="w-11/12 mx-auto mt-10 md:mt-16 mb-4 md:mb-6 grid md:grid-cols-2">
+        {items?.slice(0, 6).map((item) => (
           <MenuItemCard key={item._id} item={item}></MenuItemCard>
         ))}
       </div>
       <div className="text-center">
-        <Link to="/shop/salads">
+        <Link to="/shop/offered">
           <MenuButton buttonTitle={"Order Your Favourite Food"}></MenuButton>
         </Link>
       </div>
